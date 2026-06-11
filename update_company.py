7f7f04d26c1fa925d670209f8d321d9705b3ba90
Yashm2610 <mathury2006@gmail.com>
2026-06-11 12:36:07 +0530

    Clean up temporary scripts and patches

--- update_company.py
+++
@@ -1,8 +0,0 @@
-import app
-
-conn = app.get_db_connection()
-cursor = conn.cursor()
-cursor.execute("UPDATE company_master SET name = 'Maxworth'")
-conn.commit()
-conn.close()
-print('Updated company_master successfully')
