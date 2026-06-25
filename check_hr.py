67b9d749f2d9a4d8627f0e84ab0561b42bff510b
Yashm2610 <mathury2006@gmail.com>
2026-06-25 17:35:42 +0530

    Clean up temporary scripts

--- check_hr.py
+++
@@ -1,7 +0,0 @@
-import pymysql
-
-conn = pymysql.connect(host='localhost',user='root',password='yabh',database='employee_db',cursorclass=pymysql.cursors.DictCursor)
-c = conn.cursor()
-c.execute("SELECT id, username, role FROM users WHERE role='HR' OR username LIKE '%hr%'")
-print(c.fetchall())
-conn.close()
